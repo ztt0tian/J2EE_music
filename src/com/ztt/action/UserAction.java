8bff759d87047c0cb0e7c0192f3bf1cc984fe54c
ztt0tian <1290507445@qq.com>
2018-04-10 08:56:19 +0800

    页面登录注册初步完成

---
+++ src/com/ztt/action/UserAction.java
@@ -0,0 +1,65 @@
+package com.ztt.action;
+
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.ztt.bean.User;
+import com.ztt.services.UserServices;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import util.GenerateID;
+
+import java.util.Map;
+
+
+/**
+ * @author zhaotian
+ * @date 2018/3/13 19:45
+ */
+@Namespace("/user")
+@ParentPackage("struts-default")
+@Component("regist")
+@Scope("prototype")
+public class UserAction extends ActionSupport {
+    @Autowired
+    @Qualifier("userServices")
+    private UserServices userServices;
+    private User user;
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    @Action(value = "regist",results = {@Result(name = "success",location = "/success.jsp")})
+    public String regist() throws Exception {
+        user.setId(GenerateID.generate());
+        userServices.userRegistService(user);
+        return  "success" ;
+    }
+    @Action(value = "login",results = {
+            @Result(name = "success",location = "/index.jsp"),
+            @Result(name = "pwderror",location = "/pwderror.jsp"),
+            @Result(name = "noexist",location = "/noexist.jsp")
+    })
+    public String login() {
+        int resultcode=userServices.userLoginService(user);
+        if(resultcode==1){
+            User db_user=userServices.getUserService(user.getEmail());
+            Map<String, Object> session = ActionContext.getContext().getSession();
+            session.put("login_user",db_user);
+            return "success";
+        }
+        if(resultcode==0){
+            //密码错误
+            return "pwderror";
+        }
+        return "noexist";//用户不存在
+    }
+}
---
+++ src/logSystem/LogInterceptor.java
@@ -0,0 +1,34 @@
+package logSystem;
+
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zhaotian
+ * @date 2018/3/15 19:48
+ */
+@Aspect
+@Component
+public class LogInterceptor {
+    private final Logger logger=LoggerFactory.getLogger(LogInterceptor.class);
+    @Pointcut(value = "execution(* com.ztt.action.UserAction.*())")
+    public void controlAspect(){
+
+    }
+    @Before("controlAspect()")
+    public void doBefor(JoinPoint joinPoint){
+        System.out.println("执行前");
+    }
+    @Before("execution(* com.ztt..*(..))")
+    public void before(){
+        System.out.println("method start");
+    }
+}
